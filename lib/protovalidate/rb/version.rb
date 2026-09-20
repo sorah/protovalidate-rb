124cc1c205046131e088e919d781bce5ee6d3ef3
Sorah Fukumori <sorah@ivry.jp>
2026-09-20 09:27:34 +0900

    init

---
+++ lib/protovalidate/rb/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Protovalidate
+  module Rb
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/protovalidate/rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Protovalidate::Rb do
+  it "has a version number" do
+    expect(Protovalidate::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
